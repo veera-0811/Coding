063498dfcc676c42fff886720da84c2b5bd7cf62
veera-0811 <veerababupadala118@gmail.com>
2026-08-13 22:44:10 +0530

    Added Reorder List in linkedlist folder

---
+++ YTStriver/Linked List/LC_143. Reorder List.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        slow.next = None
+
+        prev = None
+        while second:
+            next_node = second.next
+            second.next = prev
+            prev = second
+            second = next_node
+
+        second = prev
+        first = head
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
+
+'''
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+'''
